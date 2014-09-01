a39d171b232f756646ded6801ae44bbb803c7af6
Ji Yonggang <jungleji@gmail.com>
2014-09-01 16:50:20 +0800

    Use rockchip prebuilt libstagefright.so, disable resume() and suspend() functions
    Change-Id: Iadeec141ab1fbd615e086dbcc2b478ba9609339b

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -231,6 +231,6 @@ LOCAL_MODULE:= libstagefright
 
 LOCAL_MODULE_TAGS := optional
 
-include $(BUILD_SHARED_LIBRARY)
+# include $(BUILD_SHARED_LIBRARY)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
